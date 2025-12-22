342ff6763f146483246734fce15d763dbddada19
Adrián Lancharro <adrianlancharrobayon@gmail.com>
2025-12-22 20:55:06 +0100

    modified:   src/motoFP.py 	modified:   src/motoFP_test.py 	modified:   src/motoFP.py 	modified:   src/motoFP_test.py

--- src/motoFP.py
+++ src/motoFP.py
@@ -12,30 +12,34 @@ CarreraFP=NamedTuple("CarreraFP",[
         ("podio", list[Piloto])])
 
 def lee_carreras(ruta_csv):
+    res=[]
     with open(ruta_csv, encoding="utf-8") as f:
         res=[]
         lector= csv.reader(f, delimiter=",")
         next(lector)
-        for fh, circuito, pais, seco, tiempo, nombre_ganador, marca_ganador, nombre_segundo_clasificado, marca_segundo_clasificado,nombre_tercer_clasificado, marca_tercero_clasificado in lector:
-           podio=[nombre_ganador, marca_ganador, nombre_segundo_clasificado, marca_segundo_clasificado,nombre_tercer_clasificado, marca_tercero_clasificado]
-           fecha_hora= datetime.strptime(fh, "%Y-%m-%d %H:%M")
-           
-           res.append(CarreraFP(fecha_hora, circuito, pais, seco, tiempo, podio))
-
+        for fh, circuito, pais, seco, t, nombre_ganador, marca_ganador, nombre_segundo_clasificado, marca_segundo_clasificado,nombre_tercer_clasificado, marca_tercero_clasificado in lector:
+            podio=[Piloto(nombre_ganador, marca_ganador), Piloto(nombre_segundo_clasificado, marca_segundo_clasificado), Piloto(nombre_tercer_clasificado, marca_tercero_clasificado)]
+            fecha_hora= datetime.strptime(fh, "%Y-%m-%d %H:%M")
+            if seco=="seco":
+               seco=True
+            else:
+               seco=False
+            tiempo=float(t)
+            res.append(CarreraFP(fecha_hora, circuito, pais, seco, tiempo, podio))
         return res
     
-def maximo_dias_sin_ganar(carrerasFP, piloto):
+def maximo_dias_sin_ganar(carreraFP, piloto):
+    max_dias=[]
     fechas_ganadas=[]
-    for i in carrerasFP:
-        if i.podio == piloto:
+    for i in carreraFP:
+        if i.podio[0[0]] == piloto:
             fechas_ganadas.append(i.fecha_hora)
     if len(fechas_ganadas)<2:
         return None
     else:
         sorted(fechas_ganadas)
         for f in fechas_ganadas:
-            max_dias=[]
-            dias= (f - (f-1)).days
+            dias= (fechas_ganadas[f]-fechas_ganadas[f-1]).days
             max_dias.append(dias)
         return max(max_dias)
            
--- src/motoFP_test.py
+++ src/motoFP_test.py
@@ -1,10 +1,12 @@
 from motoFP import *
 def lee_carreras_test(ruta_csv):
-    lee_carreras(ruta_csv)
+    return lee_carreras(ruta_csv)
 
-def maximo_dias_sin_ganar_test(carrerasFP, piloto):
-    print(maximo_dias_sin_ganar(carrerasFP, piloto))
+def maximo_dias_sin_ganar_test(carreraFP, piloto):
+    return maximo_dias_sin_ganar(carreraFP, piloto)
 
 if __name__== "__main__":
-    carreras= lee_carreras_test("./data/mundial_motofp.csv")
-    maximo_dias_sin_ganar_test(carreras, "Francesco Bagnaia")
+    carreras=lee_carreras_test("./data/mundial_motofp.csv")
+    print(carreras)
+    #lee_carreras_test("./data/mundial_motofp.csv")
+    print(maximo_dias_sin_ganar_test(carreras, "Marc Marquez"))
